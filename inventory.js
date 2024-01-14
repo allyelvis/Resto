dcd3df4a4924be7eca2d9760227a1d7678ad1319
allyelvis <155692151+allyelvis@users.noreply.github.com>
2024-01-14 02:35:34 +0200

    Add files via upload

---
+++ inventory.js
@@ -0,0 +1,28 @@
+From: <Saved by Blink>
+Snapshot-Content-Location: https://5bd7a2dc-4af9-4982-b042-2d4c263a7022-00-30woiz5wv6sta.riker.replit.dev/
+Subject: 
+Date: Sun, 14 Jan 2024 02:10:51 +0200
+MIME-Version: 1.0
+Content-Type: multipart/related;
+	type="text/html";
+	boundary="----MultipartBoundary--oGFqgtt4dMs5Rf8qBf1TB9AfeEQCSQonpGSVo6koS9----"
+
+
+------MultipartBoundary--oGFqgtt4dMs5Rf8qBf1TB9AfeEQCSQonpGSVo6koS9----
+Content-Type: text/html
+Content-ID: <frame-13B93303A5A7311716426D7A383DA3EA@mhtml.blink>
+Content-Transfer-Encoding: binary
+Content-Location: https://5bd7a2dc-4af9-4982-b042-2d4c263a7022-00-30woiz5wv6sta.riker.replit.dev/
+
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body>$(document).ready(function() {
+    $('#orderForm').submit(function(e) {
+        e.preventDefault();
+        alert('Thank you for placing your order! We will prepare it shortly.');
+    });
+ 
+    $('#contactForm').submit(function(e) {
+        e.preventDefault();
+        alert('Thank you for reaching out! We will get back to you soon.');
+    });
+});</body></html>
+------MultipartBoundary--oGFqgtt4dMs5Rf8qBf1TB9AfeEQCSQonpGSVo6koS9------
